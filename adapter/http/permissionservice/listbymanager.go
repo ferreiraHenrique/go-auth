957a3818bc94830b99ffe62d5080b7a3190366b1
Henrique Ferreira <henrique.fer@hotmail.com>
2023-08-29 15:13:14 -0300

    improve: structure of response endpoint

--- adapter/http/permissionservice/listbymanager.go
+++ adapter/http/permissionservice/listbymanager.go
@@ -8,8 +8,13 @@ import (
 	"github.com/gorilla/context"
 )
 
+type PermissionResponse struct {
+	UUID string `json:"uuid"`
+	Name string `json:"name"`
+}
+
 type JSONResponse struct {
-	Permissions []string `json:"permissions"`
+	Permissions []PermissionResponse `json:"permissions"`
 }
 
 func (service service) ListByManager(response http.ResponseWriter, request *http.Request) {
@@ -27,9 +32,19 @@ func (service service) ListByManager(response http.ResponseWriter, request *http
 		return
 	}
 
+	if len(*permissions) == 0 {
+		response.WriteHeader(404)
+		response.Write([]byte("permissions not found"))
+		return
+	}
+
 	resp := JSONResponse{}
 	for _, p := range *permissions {
-		resp.Permissions = append(resp.Permissions, p.Name)
+		permissionResponse := PermissionResponse{
+			UUID: p.UUID,
+			Name: p.Name,
+		}
+		resp.Permissions = append(resp.Permissions, permissionResponse)
 	}
 
 	jsonResponse, _ := json.Marshal(resp)
